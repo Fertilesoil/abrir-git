9c0b8599997bc468c94995ee040ec0d819b2bc35
Fertilesoil <gasnbloop@gmail.com>
2023-11-23 12:47:32 -0300

    Abrir git

---
+++ AbrirGithub/AbrirGithub/AutomationWeb.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbrirGithub
+{
+    internal class AutomationWeb
+    {
+        private IWebDriver? driver;
+
+        public AutomationWeb()
+        {
+            driver = new EdgeDriver();
+        }
+
+        public void TesteWeb()
+        {
+            driver!.Navigate().GoToUrl("https://github.com/login");
+            TimeSpan.FromSeconds(5);
+            driver!.FindElement(By.Name("login")).SendKeys("projetointegrador03gen@gmail.com");
+            driver!.FindElement(By.Name("password")).SendKeys("amorTeRsIatE03");
+            driver!.FindElement(By.Name("commit")).Click();
+            driver!.Navigate().GoToUrl("https://github.com/Projeto-Integrador-Grupo03/BackEnd");
+        }
+    }
+}
